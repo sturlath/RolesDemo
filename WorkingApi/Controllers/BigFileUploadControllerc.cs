bc8565f992283d2b8e12406f97f5c924f81c5b42
Sturla Þorvaldsson <sturla@ibeinni.is>
2021-12-12 14:05:36 +0000

    This code shows that normal setup works

---
+++ WorkingApi/Controllers/BigFileUploadControllerc.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WorkingApi.Controllers
+{
+    [Area("app")]
+    [ApiController]
+    [Route("api/app/bigfile")]
+    public class BigFileUploadControllerc : ControllerBase
+    {
+        [IgnoreAntiforgeryToken]
+        [DisableFormValueModelBinding]
+        [DisableRequestSizeLimit]
+        [HttpPost("upload")]
+        public async Task UploadFileAsync()
+        {
+            if (Request.ContentLength == 0)
+                BadRequest();
+
+            try
+            {
+                var filePath = Path.GetTempFileName();
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Request.StreamFile(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                //No exception!
+            }
+        }
+    }
+
+}
